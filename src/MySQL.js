9801f1b3372c8dcf43ed67c21528ec01cef39ec0
wakh <khamwa92@gmail.com>
2020-12-27 14:44:48 -0500

    Added Promises Handling

--- src/MySQL.js
+++ src/MySQL.js
@@ -1,9 +1,10 @@
-import mysql from 'mysql';
-const MySQL = mysql.createPool({
-    connectionLimit: 10,
+import mysql from 'mysql2/promise';
+
+const MySQL = mysql.createConnection({
     host: '160.153.41.3',
     user: 'myblog',
     password: '545394',
     database: 'myblog'
 });
+
 export default MySQL;
--- src/server.js
+++ src/server.js
@@ -7,45 +7,44 @@ const app = express();
 app.use(express.static(join(__dirname, '/build')));
 app.use(express.json());
 
-const resultHandle = async (err, qres, res) => {
-    if (err) res.status(500).send(err);
-    else res.status(200).send({
+const resultHandle = (res, qres) => {
+    console.log(qres);
+    res.status(200).send({
         upvotes: qres[0][0].upvotes,
         comments: qres[1]
     });
 }
 
-app.get('/api/articles/:name', (req, res) => {
-    MySQL.query({
-        sql: 'CALL GetArticle(?)',
-        values: [req.params.name], timeout: 10000
-    }, (err, qres) => resultHandle(err, qres, res));
+app.get('/api/articles/:name', async (req, res) => {
+    try {
+        const [qres, field] = await (await MySQL).execute({
+            sql: 'CALL GetArticle(?)',
+            values: [req.params.name], timeout: 10000
+        });
+        resultHandle(res, qres);
+    } catch (err) { res.status(500).send(err); }
 });
 
-app.get('/api/articles/:name/upvote', (req, res) => {
-    MySQL.query({
-        sql: 'CALL UpVote(?)',
-        values: [req.params.name], timeout: 10000
-    }, (err, qres) => resultHandle(err, qres, res));
+app.get('/api/articles/:name/upvote', async (req, res) => {
+    try {
+        const [qres, field] = await (await MySQL).execute({
+            sql: 'CALL UpVote(?)',
+            values: [req.params.name], timeout: 10000
+        });
+        resultHandle(res, qres);
+    } catch (err) { res.status(500).send(err); }
 });
 
-app.post('/api/articles/:name/add-comment', (req, res) => {
-    const { username, comment } = req.body;
-    MySQL.query({
-        sql: 'CALL AddComment(?, ?, ?)',
-        values: [req.params.name, username, comment], timeout: 10000
-    }, (err, qres) => resultHandle(err, qres, res));
+app.post('/api/articles/:name/add-comment', async (req, res) => {
+    try {
+        const { username, comment } = req.body;
+        const [qres, field] = await (await MySQL).execute({
+            sql: 'CALL AddComment(?, ?, ?)',
+            values: [req.params.name, username, comment], timeout: 10000
+        });
+        resultHandle(res, qres);
+    } catch (err) { res.status(500).send(err); }
 });
-
-app.get('/api/test', (req, res) => {
-    MySQL.query({
-        sql: 'SELECT JSON_OBJECT("id", id, "article", article, "upvotes", upvotes)git FROM articles',
-        timeout: 10000
-    }, (err, qres) => {
-        if (err) res.send(err);
-        else res.send(qres);
-    });
-})
 
 app.get('*', (req, res) => {
     res.sendFile(join(__dirname, '/build/index.html'));
